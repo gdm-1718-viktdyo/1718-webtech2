ad0e37c3b36f8b400bda036b1e07676b9b1885f9
viktoria dyo <32261059+gdm-1718-viktdyo@users.noreply.github.com>
2018-02-22 15:37:03 +0100

    commit

---
+++ week 2/oefening1/script/costum.js
@@ -0,0 +1,11 @@
+/*** Easy ***/
+
+ // Stap1  
+ let number = prompt('Geef een getal in');
+    
+    if (number % 2 == 0) {
+        document.write('Het getal is even')
+    
+    else {
+        document.write("het getal is oneven");  
+    };
---
+++ week 2/oefening3/script/custom.js
@@ -0,0 +1,27 @@
+const animal1 = 'elephant';
+const animal2 = 'cockroach';
+const animal3 = 'zebra';
+
+let animals = [];
+
+animal.push (animmal1);
+animal.push (animmal2);
+animal.push (animmal3);
+animal.push ('bunny');
+animal.push ('panter');
+
+    let allAnimals = animals[0] + animal[1] + animal[2]
+    document.write(allAnimals);
+    document.write ('<br>---------<br>');
+
+
+let allAnimals2 = '';
+
+for(let i = 0; i < animals.length; i++) {
+
+    console.log(animals [i]);
+    allAnimals2 == animals [1];
+
+}
+
+document.write(allAlnimals2);
